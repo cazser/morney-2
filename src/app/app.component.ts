76fbe999225819d078727326a230bf9a38711250
cazser <2264712072@qq.com>
2020-12-15 18:46:19 +0800

    添加路由和三个主页面，404页面

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'morney-angular';
+  state={}//这里应该是全局数据管理的地方
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavComponent } from './nav/nav.component';
+import { MoneyComponent } from './money/money.component';
+import { LabelComponent } from './label/label.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    NavComponent,
+    MoneyComponent,
+    LabelComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path:"money", component:MoneyComponent},
+      {path:"label", component:LabelComponent},
+      {path:"statistics", component:StatisticsComponent},
+      {path:"", redirectTo:"/money",  pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]

b9c591bec3ae32c2eedc321baf8b21649447af8d
cazser <2264712072@qq.com>
2020-12-16 13:54:10 +0800

    实现选择状态

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,12 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css','../commonStyle/Active.css']
 })
 export class NavComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
   }
 

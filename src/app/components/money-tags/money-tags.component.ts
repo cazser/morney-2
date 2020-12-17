cafd40581b94ef3650070e3f069fa3b00d152acc
cazser <2264712072@qq.com>
2020-12-17 15:10:33 +0800

    money-tags组件添加新增和删除标签的功能，增删改查里的改和查没实现。

--- src/app/components/money-tags/money-tags.component.ts
+++ src/app/components/money-tags/money-tags.component.ts
@@ -6,15 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./money-tags.component.css','../commonStyle/reset.css', '../commonStyle/selected.css']
 })
 export class MoneyTagsComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
   selectedTags=new Set();
   tags=['衣','食','住','行']
+  
   constructor() { }
-  tagOnClick(event:any){
-    console.log(event.target.textContent)
-    const target = event.target
-    target.classList.add("selected");
+  
+  tagOnClick(event:MouseEvent){
+    const target = event.target as Element;
+    if(this.selectedTags.has(target.textContent)){
+      this.selectedTags.delete(target.textContent);
+      target.classList.remove("selected");
+    }else{
+      this.selectedTags.add(target.textContent);
+      target.classList.add("selected");
+    }
   }
-  ngOnInit(): void {
+
+  newButton(){
+    const tag=window.prompt("请输入新标签");
+    if(tag!==null){
+      if(this.tags.indexOf(tag)>=0){
+        window.alert("标签名重复")
+      }else if(tag===''){
+        window.alert("输入为空")
+      }else{
+        this.tags.push(tag);
+      }
+    }
+  }
+  
+  deleteSelectedTags(){
+    let newTags=[]
+    for(let i=0; i<this.tags.length; i++){
+      if(!this.selectedTags.has(this.tags[i])){
+        newTags.push(this.tags[i]);
+      }
+    }
+    this.selectedTags = new Set();
+    this.tags = newTags;
   }
 
 }
